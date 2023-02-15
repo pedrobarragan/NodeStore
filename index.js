fc2d7ef7550950743518d26c8719f457a9751e31
Pedro Barragán <petergb08@gmail.com>
2023-02-14 23:07:24 -0500

    Creo mi primer get con respuesta en Json

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('My app');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res)=>{
+  res.send('Hola, soy un server en express');
+})
+
+app.get('/nueva-ruta', (peticion, respuesta)=>{
+  respuesta.json(
+    {
+      nombre: 'nombre',
+      cantidad: 2
+    }
+  )
+})
+
+app.listen(port, ()=>{
+  console.log('Mi port' + port);
+});
