b6df300513adadc20167b30ee59c0832f054d1c2
Pedro Barragán <petergb08@gmail.com>
2023-03-24 00:01:32 -0500

    listo para produccion

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes/index');
+
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ['https://myapp.co', 'http://127.0.0.1:5500'];
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin) || !origin){
+      callback(null, true);
+    }
+    else{
+      callback(new Error('No permitido'));
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res)=>{
   res.send('Hola, soy un server en express');
@@ -19,5 +34,5 @@ app.use(errorHandler);
 
 
 app.listen(port, ()=>{
-  console.log('Mi port' + port);
+  console.log('Mi port ' + port);
 });
--- services/product.service.js
+++ services/product.service.js
@@ -34,7 +34,7 @@ class ProductsService {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve(this.products);
-      }, 5000);
+      }, 500);
     })
   }
 
