a7b45362d664832f37994186dc87e658e81f0d3d
Pedro Barragán <petergb08@gmail.com>
2023-04-27 23:15:51 -0500

    Relaciones uno a uno

--- services/user.service.js
+++ services/user.service.js
@@ -11,7 +11,9 @@ class UserService {
   }
 
   async find() {
-    const rta = await models.User.findAll();
+    const rta = await models.User.findAll({
+      include: ['customer'] //Con esto le agrego la asociación al get
+    });
     return rta;
   }
 
